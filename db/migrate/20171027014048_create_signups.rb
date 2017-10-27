ace1e6a35dc0667b71b0cc80a9beb38e0bd48f2a
Debra Fong <debsfong@gmail.com>
2017-10-27 14:48:25 -0700

    fix migrations

--- db/migrate/20171027014048_create_signups.rb
+++ db/migrate/20171027014048_create_signups.rb
@@ -1,10 +1,14 @@
 class CreateSignups < ActiveRecord::Migration[5.1]
   def change
     create_table :signups do |t|
-      t.integer :event_id
-      t.integer :user_id
+      t.integer :event_id, null: false
+      t.integer :user_id, null: false
 
       t.timestamps
     end
+
+    add_index :signups, :event_id
+    add_index :signups, :user_id
+    add_index :signups, [:event_id, :user_id], unique: true
   end
 end
---
+++ db/migrate/20171027214237_create_events.rb
@@ -0,0 +1,15 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :events do |t|
+      t.integer :org_id, null: false
+      t.date :date
+      t.integer :long
+      t.integer :lat
+      t.time :start_time
+      t.time :end_time
+      t.integer :volunteers_needed
+    end
+
+    add_index :events, :org_id
+  end
+end

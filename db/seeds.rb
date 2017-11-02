0e6363422fb8907f6a9906a3b4e2a432a71851e5
Debra Fong <debsfong@gmail.com>
2017-11-02 15:15:27 -0700

    set up user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# user1 = User.new(username: "admin", password: "admin")
+user1 = User.create(username: "admin", password: "adminpassword")
+user2 = User.create(username: "guest", password: "guestpassword")

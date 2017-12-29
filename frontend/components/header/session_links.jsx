a2692932fbea0c6f5dab1a8d38d13589424441c8
Debra Fong <debsfong@gmail.com>
2017-12-28 22:37:53 -0800

    log in guest user

--- frontend/components/header/session_links.jsx
+++ frontend/components/header/session_links.jsx
@@ -15,6 +15,8 @@ class SessionLinks extends React.Component {
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.loginGuest = this.loginGuest.bind(this);
+    this.switchModal = this.switchModal.bind(this);
   }
 
   openModal(modalType) {
@@ -36,12 +38,39 @@ class SessionLinks extends React.Component {
     if (this.state.modalType === "login") {
       this.props.login({user});
     } else {
-      this.props.signup({user})
+      this.props.signup({user});
     }
   }
 
   update(field) {
     return (e) => this.setState({[field]: e.target.value});
+  }
+
+  loginGuest(e) {
+    e.preventDefault();
+    const guest = {
+      username: "guest",
+      password: "guestpassword"
+    };
+    this.props.login({user: guest});
+  }
+
+  switchModal() {
+    if (this.state.modalType === "login") {
+      return (
+        <div>
+          Don't have an account?
+          <button className="modalLink" onClick={this.openModal.bind(this, 'signup')} value="Sign Up">Sign Up</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          Already have an account?
+          <button className="modalLink" onClick={this.openModal.bind(this, 'login')} value="Log In">Log In</button>
+        </div>
+      )
+    }
   }
 
   render() {
@@ -64,17 +93,18 @@ class SessionLinks extends React.Component {
                 onChange={this.update("username")} />
             </label>
             <label>Password
-              <input type="text"
+              <input type="password"
                 placeholder="Password"
                 value={this.state.password}
                 onChange={this.update("password")} />
             </label>
             <input type="submit" value="Submit" />
+            <input type="button" value="Guest" onClick={this.loginGuest} />
           </form>
+          {this.switchModal()}
         </Modal>
       </div>
     );
-    // <input type="button" value="Guest">Guest Login</input>
   }
 }
 
